c36b1c9624921ecafe72e71b7db134da5f6dc55e
Sk Alamin <alamin695sk@gmail.com>
2024-06-25 23:53:15 +0530

    different type of function creaton

---
+++ js/function.js
@@ -0,0 +1,53 @@
+// different way of writing function-Arrow ,function,annonymous,expression function
+//1. function declaration
+function sum(n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i + 1;
+    }
+    return sum;
+}
+console.log(sum(5));
+
+//2. function expression
+const sum2 = function(n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i + 1;
+    }
+    return sum;
+}
+console.log(sum2(5));
+
+//3. Arrow function
+const sum3 = (n) => {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += i + 1;
+    }
+    return sum;
+}
+//4. annonymous function
+// Anonymous functions are functions without a name. They are often not accessible after their initial creation.
+//They can be used as arguments to other functions.
+setTimeout(function() {
+    console.log("Hello, World after 1 second!");
+  }, 1000);
+
+
+// how i can pass unlimited number of arguments to a function
+
+function sum4(...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+console.log(sum4(5, 6, 7, 8, 9, 10));
+
+function printArguments(...args) {
+    args.forEach(arg => console.log(arg));
+  }
+  
+  printArguments('Hello', 'World', 'and', 'the', 'universe');
