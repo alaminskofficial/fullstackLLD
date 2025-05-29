fc039cd682269fbae3878da301b0f587fd07fc72
Sk Alamin <skalamin@Admins-MacBook-Pro.local>
2025-05-29 10:09:30 +0530

    code added

---
+++ js/trickQ.js
@@ -0,0 +1,26 @@
+//truthy and falsy values
+// 0, "", null, undefined, NaN, false -falsy values
+// 1, " ", {}, [], true  , number , string  -truthy values
+
+let a = 12;
+if(a) {
+    console.log("a is truthy");
+}else{
+    console.log("a is falsy");
+}
+let v = "12" - 1;
+console.log(v);
+console.log(typeof v);
+let v1 = "12" - "1";
+console.log(v1);
+console.log(typeof v1);
+console.log("12" == 12)
+console.log("12" === 12)
+// diffrence between == and ===
+// == checks for value equality
+// === checks for value and type equality
+// == converts the types of the operands and then compares them
+// === does not convert the types of the operands and then compares them
+// == is not recommended to use
+
+
