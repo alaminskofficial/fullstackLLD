19555fec4b21179d1fcd2c91312a8d22b4794c3a
Sk Alamin <skalamin@Admins-MacBook-Pro.local>
2025-06-06 14:58:14 +0530

    showing in screen properly

--- react/hello-react/src/CustomHookUseCase.jsx
+++ react/hello-react/src/CustomHookUseCase.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import useFetch from './js/useFetchAlamin';
 
 const CustomHookUseCase = () => {
-  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/todos/1');
+  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/todos');
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div>
-      <h2>Data:</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h2>Todo Data:</h2>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      { data.map((todo, index) => <p key={index}>{todo.title}</p>)}
     </div>
   );
 };
